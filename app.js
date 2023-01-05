20f3166b8b33c18c5b8e22ba72e9c6fbdeb83a52
canpass <candelanadon4@gmail.com>
2023-01-05 18:59:38 -0300

    versionversion

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-let pago = prompt("¿Cual es tu metodo de pago?");
-
-switch (pago) {
-  case "efectivo":
-    console.log("Pagas en efectivo");
-    break;
-  case "tarjeta":
-    console.log("Pagas con tarjeta");
-    break;
-  case "mercadopago":
-    console.log("pagas en mercado pago");
-    break;
-  default:
-    console.log("Ninguna de las opciones anteriores");
-    break;
-}
-
-for (let i = 0; i < 10; i++) {
-  if (i === 5) {
-    break;
-  }
-  alert("$");
-}
 
 let carrito = []
 console.log(carrito);
@@ -54,6 +31,8 @@ nuevoBoton.setAttribute("class", "btn btn-danger");
 nuevoBoton.textContent = "Agregar al carrito";
 
 doocument.querySelector("#carritoo").appendChild(nuevoBoton)
+
+
 
 
 
---
+++ clase_json.js
@@ -0,0 +1,6 @@
+const enJson = '{"nombre":"MatDeYoga","unidades": 10}'
+const obj= JSON.parse(enJson)
+console.log(enJson)
+console.log(obj)
+localStorage.setItem("","")
+sessionStorage.setItem ("","")
